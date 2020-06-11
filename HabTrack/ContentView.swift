d902b7dad8a4c37a773a63f05dba201542b34d73
AF3 <af@af3.tech>
2020-06-11 02:16:34 -0400

    Initial Commit

---
+++ HabTrack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabTrack
+//
+//  Created by AL3 on 6/11/20.
+//  Copyright © 2020 AF3. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
